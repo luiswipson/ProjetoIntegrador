1d2e57efbe5490a20ee8c3aba8df828e9200a483
luiswipson <54286320+luiswipson@users.noreply.github.com>
2020-11-23 16:14:32 -0200

    Segundo Commit

--- app/Http/Controllers/TipoProdutoController.php
+++ app/Http/Controllers/TipoProdutoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\TipoProduto;
+use DB;
 
 class TipoProdutoController extends Controller
 {
@@ -14,7 +15,8 @@ class TipoProdutoController extends Controller
      */
     public function index()
     {
-        return view('TipoProduto.index');
+        $tipoProdutos = DB::select("select * from Tipo_Produtos");
+        return view('TipoProduto.index')->with('tipoProdutos' , $tipoProdutos);
     }
 
     /**
